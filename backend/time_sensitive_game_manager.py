5422611c0efcf45c3b92520703282f72e4b927d6
Caleb Golding <cg562@sussex.ac.uk>
2026-02-05 17:55:08 +0000

    updated to dict method

--- backend/time_sensitive_game_manager.py
+++ backend/time_sensitive_game_manager.py
@@ -23,16 +23,21 @@ class TimeSensitiveGameManager(UnrestrictedGameManager):
         """
         self.queue_weight: float = self.validate_config(queue_weight, lambda x: x > 0, "queue_weight", "greater than 0")
         super().__init__(team_size, p_norm, q_norm, fairness_weight, is_recording, approximate)
-        self.game_key_function: str = "priority"
+        self._match_quality_metric: str = "priority"
 
     def __repr__(self) -> str:
         return f"Team Size: {self.team_size}, P: {self.p_norm}, Q: {self.q_norm}, α: {self.fairness_weight}, β: {self.queue_weight}, Window: {self.skill_window}"
 
     def get_parameters(self) -> RecordedParameters:
-        """Get the configuration parameters of the TimeSensitiveGameManager for frontend state management."""
-        params: RecordedParameters = super().get_parameters()
-        params["queue_weight"] = self.queue_weight
-        return params
+        """Get the configuration parameters of the UnrestrictedGameManager for frontend state management."""
+        return {
+            "team_size": self.team_size,
+            "p_norm": self.p_norm,
+            "q_norm": self.q_norm,
+            "fairness_weight": self.fairness_weight,
+            "queue_weight": self.queue_weight,
+            "skill_window": self.skill_window
+        }
 
     def _create_candidate_game(self, player: Player, team_x: GameTeam, team_y: GameTeam) -> CandidateGame:
         return CandidateGame(player, team_x, team_y, self.p_norm, self.q_norm, self.fairness_weight, self.queue_weight)
